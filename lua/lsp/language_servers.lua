1a17dcd62b3bdd5fe2cef60d4d7036297012f12d
Neil Sabde <neilsabde@gmail.com>
2021-11-28 11:53:16 +0530

    Eighteenth Commit

--- lua/lsp/language_servers.lua
+++ lua/lsp/language_servers.lua
@@ -1,15 +1,15 @@
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require'lspconfig/configs'.ls_emmet = {
+require'lspconfig.configs'.ls_emmet = {
   default_config = {
-    cmd = { 'ls_emmet', '--stdio' };
-    filetypes = { 'html', 'css', 'scss' }; -- Add the languages you use, see language support
+    cmd = {'ls_emmet', '--stdio'},
+    filetypes = {'html', 'css', 'scss'}, -- Add the languages you use, see language support
     root_dir = function(_)
       return vim.loop.cwd()
-    end;
-    settings = {};
-  };
+    end,
+    settings = {}
+  }
 }
 
 local system_name
@@ -25,35 +25,29 @@ end
 
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
 local sumneko_root_path = '/Users/neilsabde/.config/something/ls/lua-language-server'
-local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-local langservers = {
-  'html',
-  'cssls',
-  'tsserver',
-  'pylsp',
-  'sumneko_lua'
-}
+local langservers = {'html', 'cssls', 'ls_emmet', 'tsserver', 'pylsp', 'sumneko_lua'}
 
 for _, server in ipairs(langservers) do
   if server == 'sumneko_lua' then
     require'lspconfig'[server].setup {
-      cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+      cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
       settings = {
         Lua = {
           runtime = {
             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
             version = 'LuaJIT',
             -- Setup your lua path
-            path = runtime_path,
+            path = runtime_path
           },
           diagnostics = {
             -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
+            globals = {'vim'}
           },
           workspace = {
             -- Make the server aware of Neovim runtime files
@@ -61,16 +55,12 @@ for _, server in ipairs(langservers) do
             checkThirdParty = false
           },
           -- Do not send telemetry data containing a randomized but unique identifier
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
+          telemetry = {enable = false}
+        }
+      }
     }
   else
-    require'lspconfig'[server].setup {
-      capabilities = capabilities
-    }
+    require'lspconfig'[server].setup {capabilities = capabilities}
   end
 end
 
