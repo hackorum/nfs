aa609c1443db3075d1000bb0f37ae368b37f6dbf
Neil Sabde <neilsabde@gmail.com>
2021-12-09 20:32:13 +0530

    Nineteenth Commit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,24 +1,24 @@
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use 'rose-pine/neovim'
-  use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
-  use {'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
-  use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
-  use {'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons'}
-  use {'windwp/nvim-ts-autotag'}
-  use {'p00f/nvim-ts-rainbow'}
-  use {'windwp/nvim-autopairs'}
-  use {'folke/which-key.nvim'}
-  use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'}}}
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
-  use 'onsails/lspkind-nvim'
-  use 'norcalli/nvim-colorizer.lua'
+  use {'rose-pine/neovim', config = "vim.cmd('colorscheme rose-pine')"}
+  use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate", event = "BufWinEnter", config = "require('treesitter-config')"}
+  use {'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}, event = "BufWinEnter", config = "require('lualine-config')"}
+  use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons', event = "BufWinEnter", config = "require('bufferline-config')"}
+  use {'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons', cmd = "NvimTreeToggle", config = "require('nvim-tree-config')"}
+  use {'windwp/nvim-ts-autotag', event = "InsertEnter", after = "nvim-treesitter"}
+  use {'p00f/nvim-ts-rainbow', after = "nvim-treesitter"}
+  use {'windwp/nvim-autopairs', config = "require('autopairs-config')", after = "nvim-cmp"}
+  use {'folke/which-key.nvim', event = "BufWinEnter", config = "require('whichkey-config')"}
+  use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'}}, cmd = "Telescope", config = "require('telescope-config')"}
+  use {'neovim/nvim-lspconfig', config = "require('lsp')"}
+  use {'hrsh7th/cmp-nvim-lsp'}
+  use {'hrsh7th/cmp-buffer'}
+  use {'hrsh7th/nvim-cmp'}
+  use {'hrsh7th/cmp-vsnip'}
+  use {'hrsh7th/vim-vsnip'}
+  use {'onsails/lspkind-nvim'}
+  use {'norcalli/nvim-colorizer.lua', config = "require('colorizer-config')", event = "BufRead"}
   use {
     'lewis6991/gitsigns.nvim',
     requires = {'nvim-lua/plenary.nvim'},
@@ -26,9 +26,9 @@ return require('packer').startup(function(use)
       require('gitsigns').setup {current_line_blame = true}
     end
   }
-  use 'glepnir/dashboard-nvim'
-  use "lukas-reineke/indent-blankline.nvim"
-  use 'lukas-reineke/format.nvim'
-  use "akinsho/toggleterm.nvim"
-  use "terrortylor/nvim-comment"
+  use {'glepnir/dashboard-nvim', cmd = "Dashboard", config = "require('dashboard-config')"}
+  use {"lukas-reineke/indent-blankline.nvim", config = "require('blankline-config')", event = "BufRead"}
+  use {'lukas-reineke/format.nvim', config = "require('format-config')", cmd = "Format"}
+  use {"akinsho/toggleterm.nvim", config = "require('toggleterm-config')"}
+  use {"terrortylor/nvim-comment", config = "require('comment-config')", cmd = "CommentToggle"}
 end)
