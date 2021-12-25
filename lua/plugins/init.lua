5dfbe8d67ff5aef2774774851e1308c00e85dc7a
Neil Sabde <neilsabde@gmail.com>
2021-12-25 19:02:47 +0530

    Fix lazy loading

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -26,10 +26,10 @@ return require('packer').startup(function(use)
       require('gitsigns').setup {current_line_blame = true}
     end
   }
-  use {'glepnir/dashboard-nvim', cmd = "Dashboard", config = "require('dashboard-config')"}
+  use {'glepnir/dashboard-nvim', event = "BufRead", config = "require('dashboard-config')"}
   use {"lukas-reineke/indent-blankline.nvim", config = "require('blankline-config')", event = "BufRead"}
-  use {'lukas-reineke/format.nvim', config = "require('format-config')", cmd = "Format"}
+  use {'lukas-reineke/format.nvim', config = "require('format-config')"}
   use {"akinsho/toggleterm.nvim", config = "require('toggleterm-config')"}
-  use {"terrortylor/nvim-comment", config = "require('comment-config')", cmd = "CommentToggle"}
-  use { 'tami5/lspsaga.nvim', config = "require('lspsaga-config')" }
+  use {"terrortylor/nvim-comment", config = "require('comment-config')"}
+  use {'tami5/lspsaga.nvim', config = "require('lspsaga-config')"}
 end)
