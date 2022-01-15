18c5a0ae312ae29b070b9f20d75b67180066dc8d
Neil Sabde <neilsabde@gmail.com>
2022-01-15 18:53:24 +0530

    Add lsp document highlight

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -17,5 +17,12 @@ null_ls.setup({
     if client.resolved_capabilities.document_formatting then
       vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
     end
+    vim.cmd [[
+      augroup document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]]
   end
 })
